7b9bef210e313ceea2341823a9d20abcf566b0e0
Nicolas Videla <nicolas.videlac@outlook.com>
2020-11-26 23:40:24 -0300

    bonito y gordito

--- enums/valuePieces.js
+++ enums/valuePieces.js
@@ -3,7 +3,7 @@ const valuePieces = {
     Horse: 30,
     Bishop: 40,
     Rook: 60,
-    Queen: 25,
+    Queen: 65, //puede que 65 sea mejor, puede
     King: 100
 }
 
--- webSocket.js
+++ webSocket.js
@@ -1,72 +1,77 @@
 //importaciones
-var W3CWebSocket  = require('websocket').w3cwebsocket;
-var Challenged = require('./responses/challenged');
-var fs = require('fs');
+const Challenged = require('./responses/challenged');
+const fs = require('fs');
 const { my_turn } = require('./responses/my_turn');
-const {makeMatriz} = require('./extras/makeMatriz');
+const { makeMatriz} = require('./extras/makeMatriz');
+const { client } = require('websocket')
 
 //lee mi authtoken de un archivo
 var authtoken = fs.readFileSync('authtoken.txt').toString();
 
-//genera el websocket
-let ws = null;
+//genera el cliente websocket
+let ws = new client();
 
-function createWS(){
-    console.log("createWS executed")
-    if(ws == null){
-        ws = new W3CWebSocket(`ws://megachess.herokuapp.com/service?authtoken=${authtoken}`)
-        console.log("ws created")
-    }
+//realiza la conexion
+function connect(){
+    ws.connect(`ws://megachess.herokuapp.com/service?authtoken=${authtoken}`)
+    console.log("Connection opened")
 }
 
-createWS();
+//ejecuto connect()
+connect();
 
-//metodo que se ejecuta cuando la conexion se establece
-ws.onopen = () => {
-    console.log('Connection opened');
-}
+//activa cuando se conecta
+ws.on('connect', function (connection) {
 
-//metodo que se ejecuta cuando la conexion se cierra
-ws.onclose = function(){
-    console.log("Connection closed");
-    ws = null;
-}
+    //accion cuando se cierra el ws
+    connection.on('close', () => {
+        console.log("Connection closed");
+        connect();
+    })
 
-//metodo que se ejecuta cuando llega un mensaje
-ws.onmessage = ({data}) => {
+    //accion cuando llega un mensaje
+    connection.on('message', (message) =>{
 
-    //convertimos el json que llega en un objeto legible
-    data = JSON.parse(data);
+        //convertimos el json que llega en un objeto legible
+        let data = JSON.parse(message.utf8Data);
 
-    //lo que realizamos depende de la accion que haya llegado en el json
-    switch (data.event){
+        //lo que realizamos depende de la accion que haya llegado en el json
+        switch (data.event){
 
-        case 'update_user_list':
-            console.log(data.data.users_list);
-            break;
+            case 'update_user_list':
+                console.log(data.data.users_list);
+                break;
 
-        case 'ask_challenge':
-            
-            //muestro quien me desafio
-            console.log("challenged by ", data.data.username)
+            case 'ask_challenge':
+                
+                //muestro quien me desafio
+                console.log("challenged by ", data.data.username)
 
-            //manda la respuesta al desafio
-            ws.send(Challenged.challenged(data.data));
-            break;
+                //manda la respuesta al desafio
+                connection.sendUTF(Challenged.challenged(data.data));
+                break;
 
-        case 'your_turn':
-            //envio el movimiento que realizo
-            ws.send(my_turn(data.data));
-            break;
+            case 'your_turn':
+                //envio el movimiento que realizo
+                connection.sendUTF(my_turn(data.data));
+                break;
 
-        case'gameover':
-            console.log(data.data);
-            console.table(makeMatriz(data.data.board))
-            break;
+            case'gameover':
+                //muestro el resultado
+                console.log(data.data);
+                //muestro el tablero
+                console.table(makeMatriz(data.data.board))
+                break;
 
-        default:
-            console.log('caso no agarrado')
-            console.log(data);
-            break;
-    }
-}
+            default:
+                console.log('caso no agarrado')
+                console.log(data);
+                break;
+        }
+    })
+})
+
+//si falla la conexion
+ws.on('connectFailed', (err) => {
+    console.log("Connection error: ", err)
+})
