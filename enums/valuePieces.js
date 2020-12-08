f989b465a9f32d5151bf7d627d8f86ad8bf2e22b
Nicolas Videla <nicolas.videlac@outlook.com>
2020-12-08 20:37:40 -0300

    improved weights

--- enums/valuePieces.js
+++ enums/valuePieces.js
@@ -1,8 +1,8 @@
 exports.valuePieces = {
-    Pawn:   25,
+    Pawn:   10,
     Horse:  30,
     Bishop: 30,
-    Rook:   30,   // entre 40 y 50
+    Rook:   50,   // entre 40 y 50
     Queen:  40,   //entre 35 y 40
     King:   100
 }
